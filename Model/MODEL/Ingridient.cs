0cb3e3715b1b33c2e327545c6da8ce266499ad9e
MrTheoslav <wo303232@student.polsl.pl>
2023-06-17 12:44:54 +0200

    Przeniesienie modeli

--- Model/MODEL/Ingridient.cs
+++ Model/MODEL/Ingridient.cs
@@ -1,6 +1,12 @@
-﻿namespace API.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.MODEL
 {
-    public class Ingridient
+    internal class Ingridient
     {
         public int IngridientID { get; set; }
         public string name { get; set; }
