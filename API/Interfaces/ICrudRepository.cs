d238f17f1314ecace393a1111e177e3c83576846
Emilia Pawela <ep303242@student.polsl.pl>
2023-06-19 13:52:51 +0200

    naprawienie interfejsu crud

--- API/Interfaces/ICrudRepository.cs
+++ API/Interfaces/ICrudRepository.cs
@@ -3,7 +3,7 @@
     public interface ICrudRepository<T>
     {
         public Task<T> getById(int id);
-        public Task<T> getAll(int page);
+        public Task<List<T>> getAll(int page);
         public Task<T> updateById(int id, T entity);
         public Task<T> deleteById(int id);
         public Task<T> add(T entity);
