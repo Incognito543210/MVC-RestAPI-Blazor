d50d145dbbc8bfbe00a90a2ced2db0ba5cb3c866
Emilia Pawela <ep303242@student.polsl.pl>
2023-06-19 13:20:11 +0200

    dodanie typów generycznych part 1

--- API/Repositories/ICrudRepository.cs
+++ API/Repositories/ICrudRepository.cs
@@ -2,6 +2,10 @@
 {
     public interface ICrudRepository<T>
     {
-        
+        public Task<T> deleteById(int id);
+
+        public Task<T> add(T entity);
+
+        public Task<T> deleteAll();
     }
 }
