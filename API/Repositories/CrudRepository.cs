7dd4555d7076f4d02d2706808b2c78e56afb536e
MrTheoslav <wo303232@student.polsl.pl>
2023-06-19 13:30:51 +0200

    Dodanie drugiej części interfejsów oraz utworzenie metod

--- API/Repositories/CrudRepository.cs
+++ API/Repositories/CrudRepository.cs
@@ -2,12 +2,31 @@
 
 namespace API.Repositories
 {
-    public class CrudRepository<T> :ICrudRepository<T> where T : class
+    public class CrudRepository<T> : ICrudRepository<T> where T : class
     {
-        public Task<T> deleteById(int id);
+        public Task<T> getById(int id)
+        {
 
-        public Task<T> add(T entity);
+        }
+        public Task<T> getAll(int page)
+        {
 
-        public Task<T> deleteAll();
+        }
+        public Task<T> updateById(int id, T entity)
+        {
+
+        }
+        public Task<T> deleteById(int id)
+        {
+
+        }
+        public Task<T> add(T entity)
+        {
+
+        }
+        public Task<T> deleteAll()
+        {
+
+        }
     }
 }
