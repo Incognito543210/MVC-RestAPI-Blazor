f637fa9f5a7b7673822032cba39652f4eff87707
Zofia Sitek <zofia.a.sitek@gmail.com>
2023-06-20 10:09:36 +0200

    jakieś zmiany

---
+++ View/Data/RecipeService.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+
+namespace View.Data
+{
+    public class RecipeService
+    {
+        private List<Recipe> recipes = new();
+
+        public void AddRecipe(Recipe recipe)
+        {
+            recipes.Add(recipe);
+        }
+        public List<Recipe> GetRecipes() {  return recipes; }
+    }
+}
