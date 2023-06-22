fee3f7a68302b3944898abc52791b318e4e19dfd
Emilia Pawela <ep303242@student.polsl.pl>
2023-06-22 19:19:28 +0200

    scalanie konfilktu

--- Model/DTO/OpinionDto.cs
+++ Model/DTO/OpinionDto.cs
@@ -11,10 +11,7 @@ namespace Model.DTO
     public class OpinionDto
     {
         public int OpinionID { get; set; }
-<<<<<<< HEAD
         public DateTime PostData { get; set; }
-=======
->>>>>>> 7ef7312830d3bfc8ad62e49bfc583f0eb3ed0027
         public string Content { get; set; }
         public int Rate { get; set; }
         public Recipe Recipe { get; set; }
