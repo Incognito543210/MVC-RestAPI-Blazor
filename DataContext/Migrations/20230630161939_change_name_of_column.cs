e0cefd23bb4368336246608b85f5ede6d8c9d344
Emilia Pawela <ep303242@student.polsl.pl>
2023-06-30 18:25:19 +0200

    migracja

---
+++ DataContext/Migrations/20230630161939_change_name_of_column.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_name_of_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Ingridients",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "Amonut",
+                table: "HasIngridients",
+                newName: "Amount");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Ingridients",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "HasIngridients",
+                newName: "Amonut");
+        }
+    }
+}
