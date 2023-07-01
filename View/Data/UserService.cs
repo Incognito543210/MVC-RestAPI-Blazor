f0d59c79c40a1cd560c0165093972a81f8cdd9a2
pardonmyfriend <eva.kedziera@gmail.com>
2023-07-01 16:52:15 +0200

    Dodanie strony do rejestracji

---
+++ View/Data/UserService.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using Model.DTO;
+using Model.MODEL;
+using System.Diagnostics;
+
+namespace View.Data
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient) 
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
