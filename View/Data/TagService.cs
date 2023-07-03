649de0e2cbf1edeaa206b70001fda130847c78bd
Zofia Sitek <zofia.a.sitek@gmail.com>
2023-07-03 19:32:33 +0200

    .

---
+++ View/Data/TagService.cs
@@ -0,0 +1,19 @@
+﻿using Model.DTO;
+
+namespace View.Data
+{
+    public class TagService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TagService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<TagDto>> GetTagsAsync()
+        {
+            return (await _httpClient.GetFromJsonAsync<List<TagDto>>("/api/Tag"))!;
+        }
+    }
+}
